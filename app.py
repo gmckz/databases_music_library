6d082f09e66d4cff0f640a854b500269d19cfa28
gmckz <gemma_mckenzie@hotmail.co.uk>
2023-09-08 14:54:55 +0100

    pushing previosuly uncommitted changes

--- app.py
+++ app.py
@@ -1,20 +1,36 @@
 from lib.database_connection import DatabaseConnection
 from lib.artist_repository import ArtistRepository
+from lib.album_repository import AlbumRepository
 
+class Application():
+    
+    def __init__(self):
+        self._connection = DatabaseConnection()
+        self._connection.connect()
+        self._connection.seed("seeds/music_library.sql")
 
-# Connect to the database
-connection = DatabaseConnection()
-connection.connect()
-
-# Seed with some seed data
-connection.seed("seeds/music_library.sql")
-
-# Retrieve all artists
-artist_repository = ArtistRepository(connection)
-artists = artist_repository.all()
-
-# List them out
-for artist in artists:
-    print(artist)
-
-print(artist_repository.find(1))
+    def run(self, choice):
+        if choice == "1":
+            album_repository = AlbumRepository(self._connection)
+            albums = album_repository.all()
+            print("\nHere is the list of albums:")
+            for album in albums:
+                print(f"{album.id} - {album.title}")
+        elif choice == "2":
+            artist_repository = ArtistRepository(self._connection)
+            artist_repository = ArtistRepository(self._connection)
+            artists = artist_repository.all()
+            print("\nHere is the list of artists:")
+            for artist in artists:
+                print(f"{artist.id} - {artist.name}")
+        else:
+            print(type(choice))
+            print("\nYou have not picked 1 or 2")
+            return None
+        
+if __name__ == '__main__':
+    app = Application()
+    print("Welcome to the music library manager!\n")
+    print("What would you like to do?")
+    choice = input("1 - List all albums\n2 - List all artists\n")
+    app.run(choice)
